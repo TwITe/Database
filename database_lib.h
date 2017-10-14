04b1d19146422f8c71d7064e2e50d8da07f18689
TwITe <dm.blink19@gmail.com>
2017-10-14 21:16:25 +0300

    update

--- database_lib.h
+++ database_lib.h
@@ -2,22 +2,18 @@
 //Call function "set_data_filesize" to set the default size of the data file
 //Call function "set_path" to set the path for saving data files
 //Call function "set_map_size" to set a size of the map that can be stored in memory before writing to file
-#pragma once
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cassert>
-#include <map>
 #include <unordered_map>
 #include <vector>
-#include <set>
 #include <cstring>
 using namespace std;
 
-struct index {
+struct index_data {
 	vector <string> file_names;
-	vector <int> start_reading_positions;
-	vector <int> end_reading_positions;
+	vector <long int> start_reading_positions;
+	vector <long int> end_reading_positions;
 	int data_size;
 	bool deleted;
 };
@@ -26,24 +22,23 @@ string user_path;
 int data_file_number = -1;
 unsigned int data_file_size = 8388608;
 unsigned int map_memory_size;
-unordered_map <int, index> indexes;
+unordered_map <int,index_data> indexes;
 
-bool check_path() {
+void check_path() {
 	if (user_path.empty()) {
 		throw runtime_error("path is invalid");
 	}
 }
 
-bool check_settings() {
+void check_settings() {
 	check_path();
-	return true;
 }
 
-void set_data_file_size(int user_data_size) {
+void set_data_file_size(unsigned int user_data_size) {
 	data_file_size = user_data_size;
 }
 
-void set_map_size(int user_map_size) {
+void set_map_size(unsigned int user_map_size) {
 	map_memory_size = user_map_size;
 }
 
@@ -63,36 +58,31 @@ bool save_current_data_size(int id, int current_data_size){
 	return true;
 }
 
-bool save_file_name_and_reading_positions(int id, const string& current_filename, int start_position, int end_position) {
+bool save_file_name_and_reading_positions(int id, const string& current_filename, long int start_position, long int end_position) {
 	indexes[id].file_names.push_back(current_filename);
 	indexes[id].start_reading_positions.push_back(start_position);
 	indexes[id].end_reading_positions.push_back(end_position);
 	return true;
 }
 
-unsigned int get_file_free_space(const string& current_file_name) {
+int get_file_free_space(const string& current_file_name) {
 	ifstream current_file(current_file_name, ifstream::binary);
 	current_file.seekg(0, ios::end);
 	if (static_cast<int>(current_file.tellg()) == 0) {
 		return data_file_size;
 	}
-	size_t current_data_file_size = current_file.tellg();
+	int current_data_file_size = static_cast<int>(current_file.tellg());
 	current_file.seekg(0, ios::beg);
 	current_file.close();
 	return data_file_size - current_data_file_size;
 }
 
 bool check_if_current_id_was_deleted(int id) {
-	if (indexes[id].deleted == true) {
-		return true;
-	}
+	return indexes[id].deleted;
 }
 
 bool check_if_current_id_is_already_exists(int id) {
-	if (indexes.count(id) == 1) {
-		return true;
-	}
-	return false;
+    return indexes.count(id) == 1;
 }
 
 void is_data_size_invalid(int current_data_size) {
@@ -101,7 +91,7 @@ void is_data_size_invalid(int current_data_size) {
 	}
 }
 
-void write_data_to_file(void* data, FILE * data_file, int last_written_byte_position, int bytes_number_to_write, const string& current_file, int id) {
+void write_data_to_file(void* data, FILE * data_file, int last_written_byte_position, size_t bytes_number_to_write, const string& current_file, int id) {
 	long int start_position = ftell(data_file);
 	fwrite(((void*)((char*)data + last_written_byte_position)), 1, bytes_number_to_write, data_file);
 	long int end_position = ftell(data_file);
@@ -114,33 +104,30 @@ void delete_data(int id) {
 }
 
 bool write_data(int id, void* data, int current_data_size) {
-	FILE * data_file = NULL;
+	FILE * data_file;
 	string current_file;
 	int last_written_byte_position = 0;
 	while (current_data_size != 0) {
-		if (data_file == NULL) {
-			current_file = get_new_file_path();
-		}
+        current_file = get_new_file_path();
 		data_file = fopen(current_file.c_str(), "a+b");
-		unsigned int file_free_space = get_file_free_space(current_file);
+		int file_free_space = get_file_free_space(current_file);
 		if (file_free_space == 0) {
 			current_file = get_new_file_path();
 			data_file = fopen(current_file.c_str(), "a+b");
 		}
-		int bytes_number_to_write;
+		size_t bytes_number_to_write;
 		if (current_data_size > file_free_space) {
-			bytes_number_to_write = file_free_space;
+			bytes_number_to_write = static_cast<size_t>(file_free_space);
 			write_data_to_file(data, data_file, last_written_byte_position, bytes_number_to_write, current_file, id);
 			last_written_byte_position += bytes_number_to_write;
 		}
 		else {
-			bytes_number_to_write = current_data_size;
+			bytes_number_to_write = static_cast<size_t>(current_data_size);
 			write_data_to_file(data, data_file, last_written_byte_position, bytes_number_to_write, current_file, id);
 		}
 		current_data_size -= bytes_number_to_write;
 		is_data_size_invalid(current_data_size);
 		fclose(data_file);
-		data_file = NULL;
 	}
 	return true;
 }
@@ -149,7 +136,7 @@ bool storage(int id, void* data, int array_length) {
 	if (check_if_current_id_is_already_exists(id)) {
 		delete_data(id);
 	}
-	unsigned int current_data_size = array_length * 4;
+	int current_data_size = array_length * 4;
 	save_current_data_size(id, current_data_size);
 	write_data(id, data, current_data_size);
 	indexes[id].deleted = false;
@@ -169,22 +156,22 @@ bool store_array_helper() {
 void* load(int id) {
 	if (check_if_current_id_was_deleted(id)) {
 		cerr << "Current data was deleted and no longer exists!";
-		return 0;
+		return nullptr;
 	}
 	FILE * data_file;
-	int current_data_size = indexes[id].data_size;
-	void* return_data = new void*[current_data_size];
-	for (int i = 0; i < indexes[id].file_names.size(); i++) {
+	size_t current_data_size = static_cast<size_t>(indexes[id].data_size);
+	void* return_data = malloc(current_data_size);
+	for (unsigned int i = 0; i < indexes[id].file_names.size(); i++) {
 		const char* current_filename = indexes[id].file_names[i].c_str();
-		int start_reading_position = indexes[id].start_reading_positions[i];
-		int end_reading_position = indexes[id].end_reading_positions[i];
+		long int start_reading_position = indexes[id].start_reading_positions[i];
+		long int end_reading_position = indexes[id].end_reading_positions[i];
 		int reading_bytes_number = end_reading_position - start_reading_position;
 		data_file = fopen(current_filename, "a+b");
-		void* current_read_data = new void*[reading_bytes_number];
+		void* current_read_data = malloc(reading_bytes_number);
 		fseek(data_file, start_reading_position, 0);
 		fread(current_read_data, 1, reading_bytes_number, data_file);
         memcpy((void*)((char*)return_data + (reading_bytes_number*i)), current_read_data, reading_bytes_number);
-		delete[] current_read_data;
+		free(current_read_data);
 		fclose(data_file);
 	}
 	return return_data;
